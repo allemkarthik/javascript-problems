e4b648a8f87268606152582781fcb3363ccf0085
allemkarthik <allemkarthik@gmail.com>
2024-10-05 17:20:43 +0530

    problem solving

---
+++ day1/age.js
@@ -0,0 +1,22 @@
+// minor or major vote task
+var age=10;
+if (age>=18){
+    console.log('you are elgible to vote');
+
+    
+}
+else{
+    console.log("you are comes under minor please wait");
+    
+}
+// even or odd
+var num=20;
+
+if (num%2==0){
+    console.log("even number");
+    
+}
+else{
+    console.log(("odd number"));
+    
+}
---
+++ day1/day1.js
@@ -0,0 +1,35 @@
+// probelm  solving
+// understanding the code
+// debugging
+// code updations
+// create a code
+
+
+// problem one Atm
+
+var insertcard="allem sai karthik";
+var atmpin="8553"
+
+var amount=100000
+
+var arr=["1.changepin","2.withdraw","3.deposit","4.check Balance"]
+
+function atm(){
+    if(insertcard=="allem sai karthik" && atmpin=="8553"){
+        console.log(arr);
+
+        option="enter your option: "
+        console.log(option);
+        
+        
+
+    }
+
+    if(option=="1.changepin"){
+        
+
+
+    }
+
+}
+atm()
